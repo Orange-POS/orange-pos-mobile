f820c62fbc1f773437497fd2003c8afbc2afa282
AchuthanDev <achuthan.dev@outlook.com>
2026-06-20 00:00:55 +0530

    Updated UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,19 +2,16 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_app/app/inventory_tracker_app.dart';
-import 'package:flutter_app/theme/app_brand.dart';
 
 void main() {
   testWidgets('OrangeONE app opens login screen', (tester) async {
     FlutterSecureStorage.setMockInitialValues({});
 
     await tester.pumpWidget(const InventoryTrackerApp());
+    await tester.pumpAndSettle();
 
-    await tester.pump();
-    await tester.pump(const Duration(milliseconds: 500));
-
-    expect(find.text(AppBrand.appName), findsOneWidget);
+    expect(find.text('Login'), findsOneWidget);
     expect(find.text('Tap to Scan'), findsOneWidget);
-    expect(find.text('Powered By'), findsOneWidget);
+    expect(find.textContaining('OrangePos'), findsOneWidget);
   });
 }
