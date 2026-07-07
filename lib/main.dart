e32b4df770d7a911b6eb40b058a51ae3c2a6359f
AchuthanDev <achuthan.dev@outlook.com>
2026-07-07 22:24:12 +0530

    Introduce app dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'app/inventory_tracker_app.dart';
 
 void main() {
-  runApp(const InventoryTrackerApp());
+  runApp(InventoryTrackerApp.production());
 }
